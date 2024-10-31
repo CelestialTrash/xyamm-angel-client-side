83620807c40f8106ef87c5a2b5334c42281a476a
Aquiles <Aquileshr92@gmail.com>
2024-10-31 18:27:07 +0100

    done

--- src/pages/Bionetta.jsx
+++ src/pages/Bionetta.jsx
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom";
 
 function Bionetta() {
     return (
-        <section className="flex items-center justify-center min-h-screen bg-black">
+        <section 
+            className="flex items-center justify-center bg-black"
+            style={{ height: 'calc(100vh - 80px - 64px)' }} // Ajusta los valores según la altura real de tu navbar y footer
+        >
             <Link
                 to="/"
                 className="text-5xl font-bold text-white transition duration-300 ease-in-out transform hover:scale-120 hover:text-white hover:[text-shadow:0_0_10px_rgba(255,255,255,0.7),_0_0_20px_rgba(255,255,255,0.7),_0_0_30px_rgba(255,255,255,0.7)]"
             >
-            Bionetta
+                Bionetta
             </Link>
         </section>
     );
